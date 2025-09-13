fa357fc09b4d88b33389a58e3e6a30f72f1afc55
NicoRomano <nicolasromano09218@gmail.com>
2025-09-13 17:55:18 -0300

    Se importan modulos nativos. Se realiza anotaciones de tipos de datos para mayor claridad. Se ajustan atributos, tipos y valores predeterminados. Mejora de getters/setters y docu. Se elimina retornos innecesarios.

--- POO-SmartHome/tests/test_evento_dispositivo.py
+++ POO-SmartHome/tests/test_evento_dispositivo.py
@@ -3,10 +3,10 @@ from evento_dispositivo import EventoDispositivo
 def test_enviar_notificacion():
     evento = EventoDispositivo()
     resultado = evento.enviar_notificacion(False)
-    assert resultado == 'El dispositivo dejo de grabar'
+    assert resultado
 
 def test_enviar_notificacion_2():
     evento = EventoDispositivo()
     resultado = evento.enviar_notificacion(True)
-    assert resultado == 'El dispositivo comenzo a grabar'
+    assert resultado
     
